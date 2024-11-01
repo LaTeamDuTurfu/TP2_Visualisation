ef89432fe817d296b0958e1e5b7ff9953d44b4a0
Joe1651 <jonathan.duplantis@hotmail.com>
2024-11-01 11:34:15 -0400

    Model Stock

---
+++ models/stock.py
@@ -0,0 +1,15 @@
+from models import db
+
+
+class Stock(db.Model):
+    __tablename__ = 'stock'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    symbol = db.Column(db.String(5), nullable=False)
+    data_year = db.Column(db.Json, nullable=False)
+    data_month = db.Column(db.Json, nullable=False)
+
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+    
