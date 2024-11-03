a14ee4b442d62858e330144a5e392171e349c768
Joe1651 <jonathan.duplantis@hotmail.com>
2024-11-03 11:59:34 -0500

    Update alphaAPI.py

--- code_tp/alphaAPI.py
+++ code_tp/alphaAPI.py
@@ -10,9 +10,10 @@ API_KEY7 = "TL1EBPOAVKFN6P56"
 API_KEY8 = "1O6BQAOX3ZK2GYH6"
 API_KEY9 = "FFAIB7TUAP5GWFOH"
 
+CURRENT_API_KEY = API_KEY0
 
 def recherche_stock(keyword):
-    url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={keyword}&apikey={API_KEY0}'
+    url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={keyword}&apikey={CURRENT_API_KEY}'
     reponse = requests.get(url)
     data = reponse.json()
 
@@ -23,12 +24,12 @@ def recherche_stock(keyword):
         return None
 
 def get_data_monthly(symbol):
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={symbol}&apikey={API_KEY0}'
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={symbol}&apikey={CURRENT_API_KEY}'
     data = requests.get(url).json()
     return data
 
 
 def get_data_30_days(symbol):
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputize=compact&apikey={API_KEY0}'
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputize=compact&apikey={CURRENT_API_KEY}'
     data = requests.get(url).json()
     return data
